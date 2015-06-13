9abde0a02e9b9db28ca6c34662d20db82e2ef50e
octopod <octopodsquad@gmail.com>
2015-06-13 13:44:32 -1000

    override getLastMessage()
    also change "extends" to "implements"

--- src/main/java/com/hyperfresh/mchyperchat/bukkit/BukkitUser.java
+++ src/main/java/com/hyperfresh/mchyperchat/bukkit/BukkitUser.java
@@ -3,17 +3,29 @@ package com.hyperfresh.mchyperchat.bukkit;
 import com.hyperfresh.mchyperchat.User;
 import org.bukkit.entity.Player;
 
-public class BukkitUser extends User
+/**
+ * An immutable wrapper for Bukkit players.
+ */
+public class BukkitUser implements User
 {
     private final Player handle;
 
-	public BukkitUser(Player player)
+	private String lastSaid;
+
+	public BukkitUser(Player player, String lastSaid)
 	{
 		this.handle = player;
+		this.lastSaid = lastSaid;
 	}
 
     public String getName()
     {
         return handle.getName();
     }
+
+	@Override
+	public String getLastMessage()
+	{
+		return lastSaid;
+	}
 }
