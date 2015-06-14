f3365af4559bb32a0183a20c89cc2c173910d84a
octopod <octopodsquad@gmail.com>
2015-06-13 14:46:21 -1000

    make BukkitPlugin implement HyperChatPlugin
    and overridden methods

--- src/main/java/com/hyperfresh/mchyperchat/bukkit/BukkitPlugin.java
+++ src/main/java/com/hyperfresh/mchyperchat/bukkit/BukkitPlugin.java
@@ -1,13 +1,32 @@
 package com.hyperfresh.mchyperchat.bukkit;
 
 import com.hyperfresh.mchyperchat.HyperChat;
+import com.hyperfresh.mchyperchat.HyperChatPlugin;
+import com.hyperfresh.mchyperchat.User;
+import org.bukkit.Bukkit;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 /**
  * The entry class for the Bukkit plugin.
  */
-public class BukkitPlugin extends JavaPlugin
+public class BukkitPlugin extends JavaPlugin implements HyperChatPlugin
 {
+	/**
+	 * A cache of Users that wrap Players.
+	 */
+	private Map<Player, User> userCache = new HashMap<>();
+
+	/**
+	 * Bukkit's console object.
+	 */
+	private User console = new BukkitConsole(Bukkit.getConsoleSender(), null);
+
     @Override
     public void onEnable()
     {
@@ -19,4 +38,24 @@ public class BukkitPlugin extends JavaPlugin
     {
         HyperChat.onDisable();
     }
+
+	@Override
+	public User getPlayer(UUID id)
+	{
+		Player player = Bukkit.getPlayer(id);
+
+		//return null if the player is null
+		return player == null ? null : getPlayer(player);
+	}
+
+	public User getPlayer(Player player)
+	{
+		return userCache.computeIfAbsent(player, p -> new BukkitUser(p, null));
+	}
+
+	@Override
+	public User getConsole()
+	{
+		return console;
+	}
 }
