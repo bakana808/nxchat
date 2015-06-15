88e8f75f926f5f823d340997d5ce4c2153b9f06b
octopod <octopodsquad@gmail.com>
2015-06-14 14:47:47 -1000

    made getPlayer return Player and add documentation

--- src/main/java/com/hyperfresh/mchyperchat/HyperChatPlugin.java
+++ src/main/java/com/hyperfresh/mchyperchat/HyperChatPlugin.java
@@ -4,7 +4,20 @@ import java.util.UUID;
 
 public interface HyperChatPlugin
 {
-	public User getPlayer(UUID id);
+	/**
+	 * Gets a Player by their UUID.
+	 *
+	 * If this method returns null, then the Player is not online.
+	 *
+	 * @param id
+	 * @return a Player or null if that player is not online
+	 */
+	public Player getPlayer(UUID id);
 
+	/**
+	 * Gets the console of the server.
+	 *
+	 * @return the console
+	 */
 	public User getConsole();
 }
