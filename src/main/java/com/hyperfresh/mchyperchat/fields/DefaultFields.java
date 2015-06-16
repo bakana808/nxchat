2edf059a8743de26d69c57258e855d7314cbfab8
octopod <octopodsquad@gmail.com>
2015-06-16 01:29:21 -1000

    added some default dynamic fields

---
+++ src/main/java/com/hyperfresh/mchyperchat/fields/DefaultFields.java
@@ -0,0 +1,37 @@
+package com.hyperfresh.mchyperchat.fields;
+
+import com.hyperfresh.mchyperchat.Field;
+import com.hyperfresh.mchyperchat.User;
+
+public class DefaultFields
+{
+	public static class Name extends Field
+	{
+		@Override
+		public String[] getFieldNames()
+		{
+			return new String[]{"name"};
+		}
+
+		@Override
+		public String getFieldValue(User sender, String... args)
+		{
+			return sender.getName();
+		}
+	}
+
+	public static class Message extends Field
+	{
+		@Override
+		public String[] getFieldNames()
+		{
+			return new String[]{"message", "msg"};
+		}
+
+		@Override
+		public String getFieldValue(User sender, String... args)
+		{
+			return sender.getLastMessage();
+		}
+	}
+}
