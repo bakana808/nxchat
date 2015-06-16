d27fe15b28de189365e5f1aad75c3bfb7dce25dc
octopod <octopodsquad@gmail.com>
2015-06-15 14:23:41 -1000

    create class HyperChatEventPoster

---
+++ src/main/java/com/hyperfresh/mchyperchat/HyperChatEventPoster.java
@@ -0,0 +1,28 @@
+package com.hyperfresh.mchyperchat;
+
+import java.util.UUID;
+
+public class HyperChatEventPoster
+{
+	/**
+	 * Executes when a player has said something.
+	 *
+	 * @param id
+	 * @param said
+	 */
+	public static void onPlayerChat(User user, String said)
+	{
+
+	}
+
+	/**
+	 * Executes only when a console has run the "say" command.
+	 * Everything <b>after</b> "say" should be included in the {@code said} argument.
+	 *
+	 * @param said what the console said.
+	 */
+	public static void onConsoleChat(String said)
+	{
+
+	}
+}
