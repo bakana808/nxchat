c184a80b73ff88d2e4bd47933795f9299fb29fc3
octopod <octopodsquad@gmail.com>
2015-06-19 06:07:18 -1000

    create enum ThemeElement

---
+++ src/main/java/com/hyperfresh/mchyperchat/ThemeElement.java
@@ -0,0 +1,21 @@
+package com.hyperfresh.mchyperchat;
+
+public enum ThemeElement
+{
+	/**
+	 * The format used at the beginning of a user's
+	 * chain of chat messages.
+	 */
+	CHAT_HEADER_FORMAT,
+
+	/**
+	 * The format used in a user's chat messages.
+	 */
+	CHAT_MESSAGE_FORMAT,
+
+	/**
+	 * The format used at the end of a user's
+	 * chain of chat messages.
+	 */
+	CHAT_FOOTER_FORMAT
+}
