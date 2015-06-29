bcadeaa010ef1a40eba74f383be8ab0871e63c94
octopod <octopodsquad@gmail.com>
2015-06-29 00:34:38 -1000

    rename Default to Optional

--- src/main/java/com/hyperfresh/mchyperchat/command/Optional.java
+++ src/main/java/com/hyperfresh/mchyperchat/command/Optional.java
@@ -10,7 +10,7 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
-public @interface Default
+public @interface Optional
 {
-	String value();
+	String[] value() default {};
 }
