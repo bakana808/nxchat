01a054036ac52b86589a37590b416efe4d664622
octopod <octopodsquad@gmail.com>
2015-06-29 00:48:19 -1000

    made running /theme without arguments list avaliable themes

--- src/main/java/com/hyperfresh/mchyperchat/HyperChatCommands.java
+++ src/main/java/com/hyperfresh/mchyperchat/HyperChatCommands.java
@@ -1,28 +1,33 @@
 package com.hyperfresh.mchyperchat;
 
 import com.hyperfresh.mchyperchat.command.Command;
+import com.hyperfresh.mchyperchat.command.Optional;
 
 public class HyperChatCommands
 {
 	HyperChat hyperChat;
 
-	public HyperChatCommands(HyperChat hyperChat)
-	{
+	public HyperChatCommands(HyperChat hyperChat) {
 		this.hyperChat = hyperChat;
 	}
 
-	@Command(permission = "hyperchat.theme.change")
-	public void theme(User user, String themeID)
-	{
+	@Command(
+		permission = "hyperchat.theme.change",
+		description = "Changes your current chat theme."
+	)
+	public void theme(User user, @Optional String themeID) {
+		if(themeID == null) {
+			user.sendMessage("Avaliable themes:");
+			for(String ID: hyperChat.getThemeManager().getThemes().keySet()) {
+				user.sendMessage("  " + ID);
+			}
+			return;
+		}
 		Theme theme = hyperChat.getThemeManager().get(themeID);
-
-		if(theme != null)
-		{
+		if(theme != null) {
 			user.setTheme(theme);
 			user.sendMessage("Theme set to " + theme.getName() + ".");
-		}
-		else
-		{
+		} else {
 			user.sendMessage("Sorry, a theme with an ID \"" + themeID + "\" does not exist.");
 		}
 	}
